667e24cfbffe89fc1a08a210672819226ae6c268
Momen Elmohder <moamenahmed25se@gmail.com>
2025-06-26 20:32:32 +0300

    splash padge added

---
+++ app/src/main/java/com/example/coffeeshop_app/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.coffeeshop_app
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.coffeeshop_app.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding= ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.startbtn.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+}
